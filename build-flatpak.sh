4e20b8bc5eed22cac242188d5db958409ff74946
Slookeur <sebastien.leroux@ipcms.unistra.fr>
2023-07-18 11:16:13 +0200

    Atomes flatpack build: mar. 18 juil. 2023 11:16:13 CEST

--- build-flatpak.sh
+++ build-flatpak.sh
@@ -7,5 +7,13 @@ fi
 git submodule add https://github.com/flathub/shared-modules.git
 
 flatpak-builder atomes org.flatpak.atomes.yml --force-clean
-#flatpak-builder --user --install --force-clean atomes org.flatpak.atomes.yml
-#flatpak run --socket=session-bus --nosocket=fallback-x11 --socket=x11 org.flatpak.atomes
+# To use the flatpak:
+# 0) Uninstall previous version
+# flatpak uninstall org.flatpak.atomes
+# 1) Install latest version
+#   1-1) If in the flatpak repo:
+#     flatpak install org.flatpak.atomes
+#   1-2) If not in the flatpak repo:
+#     flatpak-builder --user --install --force-clean atomes org.flatpak.atomes.yml
+# 2) Run flatpak
+# flatpak run --socket=session-bus --nosocket=fallback-x11 --socket=x11 org.flatpak.atomes
